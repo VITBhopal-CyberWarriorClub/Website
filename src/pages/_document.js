234af3023643c2b172a3a85a685e0b14f67994b0
flamekiller22 <flamekillerace@gmail.com>
2022-09-09 00:25:10 +0530

    update favicon and add og meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossOrigin="anonymous" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.jpg" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,12 @@ export default function Home() {
       <Head>
         <title>Cyber Warriors Club - VIT Bhopal</title>
         <meta name="description" content="A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security." />
+        <meta property="og:site_name" content="Cyber Warriors Club - VIT Bhopal" />
+        <meta property="og:title" content="Cyber Warriors Club - VIT Bhopal" />
+        <meta property="og:url" content="https://flamekiller22.github.io/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security." />
+        <meta property="og:image" content="https://cdn3.iconfinder.com/data/icons/coding-8/100/hastag-burning-coding-desktop-window-app-hashtag-burning-fire-flame-overheating-cpu-hogging-error-512.png" />
       </Head>
 
       <SiteHeader />
