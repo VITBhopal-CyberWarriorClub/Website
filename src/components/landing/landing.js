0c2b33c52036ca44b1d9830675f13bf65f94c7c8
flamekiller22 <flamekillerace@gmail.com>
2022-09-09 23:02:55 +0530

    make more small changes

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,18 +3,20 @@ import styles from './landing.module.css';
 
 export default function Landing() {
   return (
-    <div className={styles.landingContainer}>
-      <div className={styles.left}>
-        <div className={styles.heading}>
-          <h1>CyberWarriors Club</h1>
-          <h3>VIT BHOPAL</h3>
+    <div className={styles.landing}>
+      <div className={styles.landingContainer}>
+        <div className={styles.left}>
+          <div className={styles.heading}>
+            <h1>CyberWarriors Club</h1>
+            <h3>VIT BHOPAL</h3>
+          </div>
+          <p className={styles.description}>
+            A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security.
+          </p>
         </div>
-        <span className={styles.description}>
-          A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security.
-        </span>
-      </div>
-      <div className={styles.right}>
-        <Terminal />
+        <div className={styles.right}>
+          <Terminal />
+        </div>
       </div>
     </div>
   )
