2021dd747e17905ae270378acdf8af9f6c8871d5
flamekiller22 <flamekillerace@gmail.com>
2023-01-26 23:35:09 +0530

    Create event page

---
+++ src/components/event/event.js
@@ -0,0 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./event.module.css";
+import Image from "next/image";
+
+export default function EventComponent() {
+  return (
+    <div className={styles.eventcontainer}>
+      <div className={styles.heading}>
+        <h1>Cy-Hunt</h1>
+      </div>
+      <div className={styles.subheading}>
+        <h4>Challenging Quiz and Attack & Defence</h4>
+      </div>
+      <div className={styles.eventimage}>
+        <img className={styles.image} src="/event.png" alt="Event Image" />
+      </div>
+      <div className={styles.eventdescription}>
+Get ready to put your skills to the test! 🔥 <br />
+From here on, the evil 😈 gets real! <br />
+<br />
+Cyber warrior club is excited to present Cyhunt at AdVITya, the ultimate hacking competition for tech enthusiasts! 💻 <br />
+<br />
+Show off your knowledge and compete for prizes and recognition against the best in the field. 🏆  <br />
+<ul>
+  <li>Put your theoretical knowledge to the test by giving answers to some questions and qualify for the next challenge.</li>
+  <li>Get wicked and attack your opponents and become your saviour by defending your machine. </li>
+</ul>
+<br />
+<strong><u>Prize List:</u></strong> <br />
+<br />
+<ul>
+<li>
+<u>1st Place:</u>{'  '}
+Amazon Gift Voucher worth ₨.1200 <br /> <br />
+</li>
+<li>
+<u>2nd Place:</u>{'  '}
+Amazon Gift Voucher worth ₨.800 <br /> <br />
+</li>
+<li>
+<u>3rd Place:</u>{'  '}
+Amazon Gift Voucher worth ₨.400 <br /> <br />
+</li>
+</ul>
+<br />
+This February 8, 2023, get yourselves ready and join us for a day of non-stop action and excitement. Don&apos;t miss out on this opportunity of a lifetime!🔥  <br />
+Register now and be a part of the most thrilling event of the year!  <br />
+Link: <a href="https://forms.gle/kwdPHRrtcFbN3yxH6" rel="noreferrer" target="_blank">https://forms.gle/kwdPHRrtcFbN3yxH6</a> <br />
+<br />
+<span>#Advitya2023 #Cyhunt #HackingCompetition #AttackDefense</span> <br />
+<br />
+Contact Details:  <br />
+Mahendra Kumar: <a href="mailto:mahendra.kumar2020@vitbhopal.ac.in">mahendra.kumar2020@vitbhopal.ac.in</a>  <br />
+Shridhar Pandey: <a href="mailto:shridhar.pandey2020@vitbhopal.ac.in">shridhar.pandey2020@vitbhopal.ac.in</a> <br />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/event.js
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import SiteHeader from '../components/navigation/header/header'
+import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import EventComponent from '../components/event/event'
+
+export default function Home() {
+
+  const [theme, setTheme] = useState("dark");
+
+  const switchTheme = () => {
+    if (theme == "light") {
+      document.documentElement.style.setProperty("--backColor2", "black")
+      document.documentElement.style.setProperty("--backColor", "#1f1f1f")
+      document.documentElement.style.setProperty("--textColor", "white")
+      document.documentElement.style.setProperty("--headerColor", "#171717")
+
+      setTheme("dark")
+    }
+    else {
+      document.documentElement.style.setProperty("--backColor2", "#ffffff")
+      document.documentElement.style.setProperty("--backColor", "#dbdada")
+      document.documentElement.style.setProperty("--textColor", "black")
+      document.documentElement.style.setProperty("--headerColor", "#e4e4e4")
+
+      setTheme("light")
+
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cyber Warriors Club - VIT Bhopal</title>
+        <meta name="description" content="A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security." />
+        <meta property="og:site_name" content="Cyber Warriors Club - VIT Bhopal" />
+        <meta property="og:title" content="Cyber Warriors Club - VIT Bhopal" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="A community for advanced penetration testing a forum to improve your digital space skills as the world is moving to a digital age where robots and machines are doing our job and so digital security is also essential for physical security." />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_URL}/favicon.jpg`} />
+      </Head>
+
+      <SiteHeader switchTheme={switchTheme}/>
+
+      <main className={styles.main}>
+        <EventComponent />
+      </main>
+
+      {/* <footer className={styles.footer}>
+        
+      </footer> */}
+    </div>
+  )
+}
